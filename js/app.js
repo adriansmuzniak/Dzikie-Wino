f0aef6232270fa011e1a36b4adf31c1e4059f04e
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-06-30 10:59:26 +0200

    logo fading

--- js/app.js
+++ js/app.js
@@ -6,37 +6,48 @@ console.log();
 
 $(".background").hover(
   function(){
-    $(".section-button").stop(true, true).fadeOut(700, "swing" );
+    $(".section-button").stop(true, true).fadeOut(500, "swing" );
     $(".background").stop(true, true).animate({"opacity": "0.9", "height": "-=150px"},700);
-    // $(".span-text").stop().animate({"font-size": "+=3px"}, "slow");
+    // $(".span-text").stop().fadeOut(1000);
   },
   function(){
-        // $(".span-text").stop().animate({"font-size": "-=3px"}, "slow");
-    // $(".span-text").stop().slideDown("slow");
+    // $(".span-text").stop().fadeIn(1000);
     $(".background").stop(true, true).animate({"opacity": "1", "height": "+=150px"},300);
     $(".section-button").stop(true, true).fadeIn(700, "swing");
 
   }
 )
 
+$("#slider").hover(
+  function(){
+    $("#logo").stop(true,true).animate({"height": "155px", "width": "170px","opacity": "0.2"},1);
+    $("#slider").stop(true,true).animate({"z-index": "+=1"},200);
+  },
+  function(){
+    $("#logo").stop(true,true).animate({"height": "180px", "width": "200px", "opacity": "1" },1);
+      $("#slider").stop(true,true).animate({"z-index": "-=1"},1);
+  }
+)
+
+// ----------------------------------------------------------------
 $(function(){
 
 // configuration
- var width = 700;
- var animationSpeed = 1000;
- var pause = 3000;
+ var width = 700; //szerokość
+ var animationSpeed = 500; //szybkość animacji - 1s
+ var pause = 3000; //szybkość pętli
  var currentSlide = 1;
 
 //cacheDOM
- var $slider = $("#slider");
- var $slideContainer = $slider.find(".slides");
- var $slides = $slideContainer.find(".slide");
+ var $slider = $("#slider"); //div w którym jest slider
+ var $slideContainer = $slider.find(".slides"); // ul
+ var $slides = $slideContainer.find(".slide"); // li
 
- var interval;
+ var interval; // interval na zewnątrz - scope
 
  function startSlider(){
    interval = setInterval(function(){
-     $slideContainer.animate({"margin-left": "-="+width}, animationSpeed, function() {
+     $slideContainer.animate({"margin-left": "-="+ width},  animationSpeed, function() {
        currentSlide++;
        if (currentSlide === $slides.length) {
          currentSlide = 1;
@@ -46,11 +57,13 @@ $(function(){
    }, pause);
  }
 
+//pauza po najechaniu
  function pauseSlider(){
   clearInterval(interval);
   }
 
-  startSlider();
+//start po najechaniu
+  startSlider(); //wywołanie funkcji
   $slider.on("mouseenter", pauseSlider).on("mouseleave", startSlider);
 
   })
